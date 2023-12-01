34fbbb4799a17b7b2b171ce0ae9cb91609d1c018
Chris Millar <chris@millr.org>
2023-12-01 14:47:18 -0700

    Update aem.js

--- scripts/aem.js
+++ scripts/aem.js
@@ -307,8 +307,8 @@ function createOptimizedPicture(
   eager = false,
   breakpoints = [{ media: '(min-width: 600px)', width: '2000' }, { width: '750' }],
 ) {
-  console.log(src);
-  const url = new URL(src, window.location.href);
+  const base = !src.startsWith('http') ? window.location.href : null;
+  const url =  ? new URL(src, window.location.href);
   const picture = document.createElement('picture');
   const { pathname } = url;
   const ext = pathname.split('.').pop();
